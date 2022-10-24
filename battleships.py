9d877d405c365ea6353955ed7ff3f131c8fc2aae
Rustic-Citrus <mrcurtis.english@gmail.com>
2022-10-24 20:31:39 -0300

    commit

--- battleships.py
+++ battleships.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from tabulate import tabulate
 import string
+import random
+import warnings
 
 letters = string.ascii_uppercase
+
+directions = ["left", "right", "up", "down"]
 
 board = {
 	"A": {1: "-", 2: "-", 3: "-", 4: "-", 5: "-", 6: "-", 7: "-", 8: "-", 9: "-", 10: "-"},
@@ -20,14 +24,17 @@ board = {
 ships = {
 	"aircraft_carrier": {
 		"on_board": False,
+		"symbol": "A",
 		"hits": 0
 	},
 	"battleship": {
 		"on_board": False,
+		"symbol": "B",
 		"hits": 0
 	},
 	"cruiser": {
 		"on_board": False,
+		"symbol": "C",
 		"hits": 0
 	},
 	"destroyer": {
@@ -39,6 +46,7 @@ ships = {
 			"on_board": False,
 			"hits": 0
 		},
+		"symbol": "D",
 		"count": 0
 	},
 	"submarine": {
@@ -50,17 +58,31 @@ ships = {
 			"on_board": False,
 			"hits": 0
 			},
+		"symbol": "S",
 		"count": 0
 	}
 }
+
+def get_occupied_coordinates():
+
+	occupied_coordinates = []
+
+	for m, n in board.items():
+
+		for i, j in n.items():
+
+			if j in [ships["aircraft_carrier"]["symbol"], ships["battleship"]["symbol"], ships["cruiser"]["symbol"], ships["destroyer"]["symbol"], ships["submarine"]["symbol"]]:
+
+				coordinate = m + str(i)
+				occupied_coordinates.append(coordinate)
+
+	return occupied_coordinates
 
 def get_board():
 	
 	print("=== BOARD ===")
 	df = pd.DataFrame(board)
 	print(tabulate(df, headers = "keys", tablefmt = "psql"))
-
-# get_board()
 
 def fire(coord):
 
@@ -86,185 +108,451 @@ def fire(coord):
 
 	get_board()
 
-# fire("F3")
-
 def set_aircraft_carrier(coord, direction):
-
-	directions = ["left", "right", "up", "down"]
 
 	if ships["aircraft_carrier"]["on_board"] == True:
 
-		print("Cannot place! There is already an aircraft carrier on the board.")
+		warnings.warn("Invalid coordinate-direction combo")
 		return
 
 	else:
 
-		ships["aircraft_carrier"]["on_board"] = True
+		if type(coord) == str:
 
-	if type(coord) == str:
+			try:
 
-		try:
+				x = coord[0].upper()
+				y = int(coord[1])
 
-			x = coord[0].upper()
-			y = int(coord[1])
+			except:
 
-		except:
+				warnings.warn("Invalid coordinate format")
 
-			print("Make sure you type a coordinate, e.g. A1 or B2")
+		if coord in get_occupied_coordinates():
 
-	if direction.lower() in directions:
+			warnings.warn("Coordinates already occupied")
+			return
 
-			if direction.lower() == "left":
-				count = letters.find(x)
-				for n in range(5):
-					board[letters[count]][y] = 1
-					count -= 1
+		if direction.lower() in directions:
 
-			elif direction.lower() == "right":
-				count = letters.find(x)
-				for n in range(5):
-					board[letters[count]][y] = 1
-					count += 1
+			if (x in ["G", "H", "I", "J"]) and (direction == "right"):
 
-			elif direction.lower() == "up":
-				count = y
-				for n in range(5):
-					board[x][count] = 1
-					count -= 1
+				warnings.warn("Invalid coordinates and direction")
+				return
 
-			elif direction.lower() == "down":
-				count = y
-				for n in range(5):
-					board[x][count] = 1
-					count += 1
+			elif (x in ["A", "B", "C", "D"]) and (direction == "left"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y > 6) and (direction == "down"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y < 5) and (direction == "up"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			else:
+
+				if direction.lower() == "left":
+					count = letters.find(x)
+					for n in range(5):
+						board[letters[count]][y] = ships["aircraft_carrier"]["symbol"]
+						count -= 1
+					ships["aircraft_carrier"]["on_board"] = True
+
+				elif direction.lower() == "right":
+					count = letters.find(x)
+					for n in range(5):
+						board[letters[count]][y] = ships["aircraft_carrier"]["symbol"]
+						count += 1
+					ships["aircraft_carrier"]["on_board"] = True
+
+				elif direction.lower() == "up":
+					count = y
+					for n in range(5):
+						board[x][count] = ships["aircraft_carrier"]["symbol"]
+						count -= 1
+					ships["aircraft_carrier"]["on_board"] = True
+
+				elif direction.lower() == "down":
+					count = y
+					for n in range(5):
+						board[x][count] = ships["aircraft_carrier"]["symbol"]
+						count += 1
+					ships["aircraft_carrier"]["on_board"] = True
 
 	get_board()
-
-# set_aircraft_carrier("A1", "right")
 
 def set_battleship(coord, direction):
 
-	directions = ["left", "right", "up", "down"]
+	if ships["battleship"]["on_board"] == True:
 
-	if type(coord) == str:
+		warnings.warn("Invalid coordinate-direction combo")
+		return
 
-		try:
+	else:
 
-			x = coord[0].upper()
-			y = int(coord[1])
+		if type(coord) == str:
 
-		except:
+			try:
 
-			print("Make sure you type a coordinate, e.g. A1 or B2")
+				x = coord[0].upper()
+				y = int(coord[1])
 
-	if direction.lower() in directions:
+			except:
 
-		if direction.lower() == "left":
-			count = letters.find(x)
-			for n in range(4):
-				board[letters[count]][y] = 1
-				count -= 1
+				warnings.warn("Invalid coordinate format")
 
-		elif direction.lower() == "right":
-			count = letters.find(x)
-			for n in range(4):
-				board[letters[count]][y] = 1
-				count += 1
+		if coord in get_occupied_coordinates():
 
-		elif direction.lower() == "up":
-			count = y
-			for n in range(4):
-				board[x][count] = 1
-				count -= 1
+			warnings.warn("Coordinates already occupied")
+			return
 
-		elif direction.lower() == "down":
-			count = y
-			for n in range(4):
-				board[x][count] = 1
-				count += 1
+		if direction.lower() in directions:
+
+			if (x in ["H", "I", "J"]) and (direction == "right"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (x in ["A", "B", "C"]) and (direction == "left"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y > 7) and (direction == "down"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y < 4) and (direction == "up"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			else:
+
+				if direction.lower() == "left":
+					count = letters.find(x)
+					for n in range(4):
+						board[letters[count]][y] = ships["battleship"]["symbol"]
+						count -= 1
+					ships["battleship"]["on_board"] = True
+
+				elif direction.lower() == "right":
+					count = letters.find(x)
+					for n in range(4):
+						board[letters[count]][y] = ships["battleship"]["symbol"]
+						count += 1
+					ships["battleship"]["on_board"] = True
+
+				elif direction.lower() == "up":
+					count = y
+					for n in range(4):
+						board[x][count] = ships["battleship"]["symbol"]
+						count -= 1
+					ships["battleship"]["on_board"] = True
+
+				elif direction.lower() == "down":
+					count = y
+					for n in range(4):
+						board[x][count] = ships["battleship"]["symbol"]
+						count += 1
+					ships["battleship"]["on_board"] = True
 
 	get_board()
 
-# set_battleship("A5", "right")
-
 def set_cruiser(coord, direction):
 
-	directions = ["left", "right", "up", "down"]
+	if ships["cruiser"]["on_board"] == True:
 
-	if type(coord) == str:
+		warnings.warn("Invalid coordinate-direction combo")
+		return
 
-		try:
+	else:
 
-			x = coord[0].upper()
-			y = int(coord[1])
+		if type(coord) == str:
 
-		except:
+			try:
 
-			print("Make sure you type a coordinate, e.g. A1 or B2")
+				x = coord[0].upper()
+				y = int(coord[1])
 
-	if direction.lower() in directions:
+			except:
 
-			if direction.lower() == "left":
-				count = letters.find(x)
-				for n in range(3):
-					board[letters[count]][y] = 1
-					count -= 1
+				warnings.warn("Invalid coordinate format")
 
-			elif direction.lower() == "right":
-				count = letters.find(x)
-				for n in range(3):
-					board[letters[count]][y] = 1
-					count += 1
+		if coord in get_occupied_coordinates():
 
-			elif direction.lower() == "up":
-				count = y
-				for n in range(3):
-					board[x][count] = 1
-					count -= 1
+			warnings.warn("Coordinates already occupied")
+			return
 
-			elif direction.lower() == "down":
-				count = y
-				for n in range(3):
-					board[x][count] = 1
-					count += 1
+		if direction.lower() in directions:
 
-def set_destroyer(coord, direction):
+			if (x in ["I", "J"]) and (direction == "right"):
 
-	directions = ["left", "right", "up", "down"]
+				warnings.warn("Invalid coordinates and direction")
+				return
 
-	if type(coord) == str:
+			elif (x in ["A", "B"]) and (direction == "left"):
 
-		try:
+				warnings.warn("Invalid coordinates and direction")
+				return
 
-			x = coord[0].upper()
-			y = int(coord[1])
+			elif (y > 8) and (direction == "down"):
 
-		except:
+				warnings.warn("Invalid coordinates and direction")
+				return
 
-			print("Make sure you type a coordinate, e.g. A1 or B2")
+			elif (y < 3) and (direction == "up"):
 
-	if direction.lower() in directions:
+				warnings.warn("Invalid coordinates and direction")
+				return
 
-			if direction.lower() == "left":
-				count = letters.find(x)
-				for n in range(2):
-					board[letters[count]][y] = 1
-					count -= 1
+			else:
 
-			elif direction.lower() == "right":
-				count = letters.find(x)
-				for n in range(2):
-					board[letters[count]][y] = 1
-					count += 1
+				if direction.lower() == "left":
+					count = letters.find(x)
+					for n in range(3):
+						board[letters[count]][y] = ships["cruiser"]["symbol"]
+						count -= 1
+					ships["cruiser"]["on_board"] = True
 
-			elif direction.lower() == "up":
-				count = y
-				for n in range(2):
-					board[x][count] = 1
-					count -= 1
+				elif direction.lower() == "right":
+					count = letters.find(x)
+					for n in range(3):
+						board[letters[count]][y] = ships["cruiser"]["symbol"]
+						count += 1
+					ships["cruiser"]["on_board"] = True
 
-			elif direction.lower() == "down":
-				count = y
-				for n in range(2):
-					board[x][count] = 1
-					count += 1
+				elif direction.lower() == "up":
+					count = y
+					for n in range(3):
+						board[x][count] = ships["cruiser"]["symbol"]
+						count -= 1
+					ships["cruiser"]["on_board"] = True
+
+				elif direction.lower() == "down":
+					count = y
+					for n in range(3):
+						board[x][count] = ships["cruiser"]["symbol"]
+						count += 1
+					ships["cruiser"]["on_board"] = True
+
+	get_board()
+
+def set_destroyer_one(coord, direction):
+
+	if ships["destroyer"]["destroyer_one"]["on_board"] == True:
+
+		warnings.warn("Invalid coordinate-direction combo")
+		return
+
+	else:
+
+		if type(coord) == str:
+
+			try:
+
+				x = coord[0].upper()
+				y = int(coord[1])
+
+			except:
+
+				warnings.warn("Invalid coordinate format")
+
+		if coord in get_occupied_coordinates():
+
+			warnings.warn("Coordinates already occupied")
+			return
+
+		if direction.lower() in directions:
+
+			if (x == "J") and (direction == "right"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (x == "A") and (direction == "left"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y > 9) and (direction == "down"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y < 2) and (direction == "up"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			else:
+
+				if direction.lower() == "left":
+					count = letters.find(x)
+					for n in range(2):
+						board[letters[count]][y] = ships["destroyer"]["symbol"]
+						count -= 1
+					ships["destroyer"]["destroyer_one"]["on_board"] = True
+
+				elif direction.lower() == "right":
+					count = letters.find(x)
+					for n in range(2):
+						board[letters[count]][y] = ships["destroyer"]["symbol"]
+						count += 1
+					ships["destroyer"]["destroyer_one"]["on_board"] = True
+
+				elif direction.lower() == "up":
+					count = y
+					for n in range(2):
+						board[x][count] = ships["destroyer"]["symbol"]
+						count -= 1
+					ships["destroyer"]["destroyer_one"]["on_board"] = True
+
+				elif direction.lower() == "down":
+					count = y
+					for n in range(2):
+						board[x][count] = ships["destroyer"]["symbol"]
+						count += 1
+					ships["destroyer"]["destroyer_one"]["on_board"] = True
+
+	get_board()
+
+def set_destroyer_two(coord, direction):
+
+	if ships["destroyer"]["destroyer_two"]["on_board"] == True:
+
+		warnings.warn("Invalid coordinate-direction combo")
+		return
+
+	else:
+
+		if type(coord) == str:
+
+			try:
+
+				x = coord[0].upper()
+				y = int(coord[1])
+
+			except:
+
+				warnings.warn("Invalid coordinate format")
+
+		if coord in get_occupied_coordinates():
+
+			warnings.warn("Coordinates already occupied")
+			return
+
+		if direction.lower() in directions:
+
+			if (x == "J") and (direction == "right"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (x == "A") and (direction == "left"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y > 9) and (direction == "down"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			elif (y < 2) and (direction == "up"):
+
+				warnings.warn("Invalid coordinates and direction")
+				return
+
+			else:
+
+				if direction.lower() == "left":
+					count = letters.find(x)
+					for n in range(2):
+						board[letters[count]][y] = ships["destroyer"]["symbol"]
+						count -= 1
+					ships["destroyer"]["destroyer_two"]["on_board"] = True
+
+				elif direction.lower() == "right":
+					count = letters.find(x)
+					for n in range(2):
+						board[letters[count]][y] = ships["destroyer"]["symbol"]
+						count += 1
+					ships["destroyer"]["destroyer_two"]["on_board"] = True
+
+				elif direction.lower() == "up":
+					count = y
+					for n in range(2):
+						board[x][count] = ships["destroyer"]["symbol"]
+						count -= 1
+					ships["destroyer"]["destroyer_two"]["on_board"] = True
+
+				elif direction.lower() == "down":
+					count = y
+					for n in range(2):
+						board[x][count] = ships["destroyer"]["symbol"]
+						count += 1
+					ships["destroyer"]["destroyer_two"]["on_board"] = True
+
+	get_board()
+
+def set_ai_positions():
+
+	direction = directions[random.randint(0, 3)]
+	row = letters[random.randint(0, 9)]
+	column = random.randint(1, 10)
+	coord = row + str(column)
+	
+	while ships["aircraft_carrier"]["on_board"] == False:
+
+		set_aircraft_carrier(coord, direction)
+
+		direction = directions[random.randint(0, 3)]
+		row = letters[random.randint(0, 9)]
+		column = random.randint(1, 10)
+		coord = row + str(column)
+
+	while ships["battleship"]["on_board"] == False:
+
+		set_battleship(coord, direction)
+
+		direction = directions[random.randint(0, 3)]
+		row = letters[random.randint(0, 9)]
+		column = random.randint(1, 10)
+		coord = row + str(column)
+
+	while ships["cruiser"]["on_board"] == False:
+
+		set_cruiser(coord, direction)
+
+		direction = directions[random.randint(0, 3)]
+		row = letters[random.randint(0, 9)]
+		column = random.randint(1, 10)
+		coord = row + str(column)
+
+	while ships["destroyer"]["destroyer_one"]["on_board"] == False:
+
+		set_destroyer_one(coord, direction)
+
+		direction = directions[random.randint(0, 3)]
+		row = letters[random.randint(0, 9)]
+		column = random.randint(1, 10)
+		coord = row + str(column)
+
+	while ships["destroyer"]["destroyer_two"]["on_board"] == False:
+
+		set_destroyer_two(coord, direction)
+
+		direction = directions[random.randint(0, 3)]
+		row = letters[random.randint(0, 9)]
+		column = random.randint(1, 10)
+		coord = row + str(column)
+
+set_ai_positions()
